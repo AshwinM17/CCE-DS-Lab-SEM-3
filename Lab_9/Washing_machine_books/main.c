c511e70b2ad19653e904d2a6f63df6115b13e36c
Sherlock <82929505+AvMavs@users.noreply.github.com>
2024-10-15 09:38:26 +0530

    Update main.c

--- Lab_9/Washing_machine_books/main.c
+++ Lab_9/Washing_machine_books/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 typedef struct cnode *cnptr;
 typedef struct cnode
 {
@@ -8,71 +9,83 @@ typedef struct cnode
     char name[100];
     cnptr next;
     cnptr prev;
-}cnode;
-cnptr last=NULL;
+} cnode;
+
+cnptr last = NULL;
+
 cnptr createCnode(int val)
 {
-    cnptr temp=(cnptr)malloc(sizeof(cnode));
-    temp->data=val;
-    temp->next=NULL;
-    temp->prev=NULL;
+    cnptr temp = (cnptr)malloc(sizeof(cnode));
+    temp->data = val;
+    temp->next = NULL;
+    temp->prev = NULL;
     return temp;
 }
-void insertBegin(int val,char nam[])
+
+void insertBegin(int val, char nam[])
 {
-    cnptr temp=createCnode(val);
-    strcpy(temp->name,nam);
-    if(last==NULL)
+    cnptr temp = createCnode(val);
+    strcpy(temp->name, nam);
+    if (last == NULL)
     {
-        last=temp;
-        last->next=last;
-        last->prev=last;
+        last = temp;
+        last->next = last;
+        last->prev = last;
     }
     else
     {
-        temp->next=last->next;//head
-        last->next->prev=temp;
-        temp->prev=last;
-        last->next=temp;
+        temp->next = last->next; // head
+        last->next->prev = temp;
+        temp->prev = last;
+        last->next = temp;
     }
 }
+
 void deleteend()
 {
-    cnptr current;
-    if(last==NULL)
+    if (last == NULL)
     {
-        printf("Queue is empty");
+        printf("Queue is empty\n");
     }
     else
     {
-        printf("Next person is %s for %d mins\n",last->name,last->data);
-        cnptr previous=last->prev;
-        previous->next=last->next;
-        last->next->prev=previous;
+        printf("Next person is %s for %d mins\n", last->name, last->data);
+        cnptr previous = last->prev;
+        previous->next = last->next;
+        last->next->prev = previous;
         free(last);
-        last=previous;
+        last = previous;
+
+        if (last->next == last) // If the list becomes empty
+            last = NULL;
     }
 }
+
 void traverse()
 {
-    cnptr current=last;
-    current=current->next;
-    while(current!=last)
-    {
-        printf("%s ",current->name);
-        current=current->next;
+    if (last == NULL) {
+        printf("Queue is empty\n");
+        return;
     }
-    printf("%s\n",current->name);
+
+    cnptr current = last->next;
+    do {
+        printf("%s (%d mins) ", current->name, current->data); // Print name AND time
+        current = current->next;
+    } while (current != last->next);
+
+    printf("\n");
 }
+
 int main()
 {
-    char contin='y';
+    char contin = 'y';
     int choice;
-    while(contin=='y')
+    while (contin == 'y')
     {
-        printf("What do you want to do?1 adding person to queue,2)give the chance to next person,3) to view the Queue\n");
-        scanf("%d",&choice);
-        switch(choice)
+        printf("What do you want to do? \n 1. Add person to queue \n 2. Pass the chance to next person \n 3. View the Queue\n");
+        scanf("%d", &choice);
+        switch (choice)
         {
         case 1:
             {
@@ -81,9 +94,9 @@ int main()
                 fflush(stdin);
                 char name[100];
                 gets(name);
-                printf("Enter the time in mins\n");
-                scanf("%d",&val);
-                insertBegin(val,name);
+                printf("Enter the time in minutes: \n");
+                scanf("%d", &val);
+                insertBegin(val, name);
                 break;
             }
         case 2:
@@ -93,16 +106,15 @@ int main()
             }
         case 3:
             {
-                printf("people in the Queue are\n");
+                printf("People in the Queue are\n");
                 traverse();
                 break;
             }
         }
-        printf("continue?(y/n)\n");
+        printf("continue? (y/n)\n");
         fflush(stdin);
-        scanf("%c",&contin);
+        scanf(" %c", &contin);
     }
 
+    return 0;
 }
-
-
